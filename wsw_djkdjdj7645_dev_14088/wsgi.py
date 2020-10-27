b85a7f230e5c47b6643e231419bdc16d931bcf48
Crowdbotics <team@crowdbotics.com>
2020-10-27 06:24:48 +0000

    Initial commit.

---
+++ wsw_djkdjdj7645_dev_14088/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for wsw_djkdjdj7645_dev_14088 project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/2.2/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'wsw_djkdjdj7645_dev_14088.settings')
+
+application = get_wsgi_application()
